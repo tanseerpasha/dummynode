e99031bf68ccdf3772685d976aa2f43d83d72473
mtp <tanseerpasha@gmail.com>
2023-11-12 11:51:16 +0530

    update files

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 const PORT = process.env.PORT || 3030;
 
 // your code
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
