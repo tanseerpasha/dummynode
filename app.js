994911da2596c0208bdee8813688599b96cd0636
mtp <tanseerpasha@gmail.com>
2023-11-13 11:16:41 +0530

    post 1

--- app.js
+++ app.js
@@ -10,35 +10,6 @@ const PORT = process.env.PORT || 3030;
 
 const privateKey = fs.readFileSync('AuthKey_L38ADHKU82.p8')
 
-const token = jwt.sign({
-  sub: 'com.pa.myweatherkit'
-}, privateKey, {
-  // jwtid: 'T4W24SQJKG.com.pa.myweatherkit',
-  issuer: 'T4W24SQJKG',
-  expiresIn: '1hr',
-  keyid: 'L38ADHKU82',
-  algorithm: 'ES256',
-  header: {
-      id: 'T4W24SQJKG.com.pa.myweatherkit'
-  }
-})
-
-app.get('/token', async (req, res) =>{
-  console.log(privateKey)
-  console.log(token)
-  console.log("tanseer")
-
-  const url = 'https://weatherkit.apple.com/api/v1/availability/37.323/122.032'
-
-  const configuration = {
-      headers: {Authorization : 'Bearer ' + token}
-  }
-
-
-  const result = await axios.get(url, configuration)
-  res.send(token)
-
-})
 
 
 // your code
@@ -53,11 +24,31 @@ app.post('/', (req, res) => {
 })
 
 
-//ensure json is slected in postman
-//body/raw/json
-app.post('/api/customer', (req, res) => {
-  res.send(req.body)
-  console.log(req.body)
+// {
+//   sub: 'com.pa.myweatherkit',
+//   issuer: 'T4W24SQJKG',
+//   expiresIn: '1hr',
+//   keyid: 'L38ADHKU82',
+//   algorithm: 'ES256',
+//   hId: 'T4W24SQJKG.com.pa.myweatherkit'
+// }
+
+app.post('/getToken', (req, res) => {
+  const body = req.body
+  const token = jwt.sign({
+    sub: body.sub
+  }, privateKey, {
+    // jwtid: 'T4W24SQJKG.com.pa.myweatherkit',
+    issuer: body.issuer,
+    expiresIn: body.expiresIn,
+    keyid: body.keyid,
+    algorithm: body.algorithm,
+    header: {
+        id: body.hId
+    }
+  })
+  res.send(token)
+  // console.log(token)
 })
 
 app.listen(PORT, () => {
